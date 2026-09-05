ba681e480e675fe0ddf09c3624e08252ed32b355
Fatima Zahra Azzabi <fatimazahraazzabi24@gmail.com>
2026-09-06 00:38:03 +0100

    fix: web build successful locally, pushing corrected dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
-import file_picker_darwin
+import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
-import record_macos
+import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
